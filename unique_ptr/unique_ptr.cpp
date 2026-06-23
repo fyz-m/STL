30b73baeedffe212bb28c60764acb466441154fd
fyz.m <237607300+fyz-m@users.noreply.github.com>
2026-06-23 11:29:27 +0400

    Update format

--- unique_ptr/unique_ptr.cpp
+++ unique_ptr/unique_ptr.cpp
@@ -3,41 +3,37 @@
 
 namespace lib {
 
-template<typename T>
+template <typename T>
 class unique_ptr {
 
-private:
+  private:
+    T* m_ptr;
 
-     T* m_ptr;
-
-public:
-
+  public:
     // Delete the copy constructor
     unique_ptr(const unique_ptr& ptr) = delete;
 
-    // Delete copy assignment 
+    // Delete copy assignment
     void operator=(const unique_ptr& ptr) = delete;
 
     // Move constructor
     // Transfer ownership of resources
-    unique_ptr(unique_ptr&& other) 
-            : m_ptr{other.get()}
-        {
-            other.m_ptr = nullptr;
-        }
-
-    // Constructor from raw pointer
-    explicit unique_ptr(T* other) : m_ptr{other} {}
-
-    explicit unique_ptr() : m_ptr{nullptr} {} 
-    
-
-    // Return managed pointer
-    T* get() const {
-        return m_ptr;
+    unique_ptr(unique_ptr&& other)
+        : m_ptr{other.get()} {
+        other.m_ptr = nullptr;
     }
 
-    // Release ownership of pointer (no longer automatically managed) 
+    // Constructor from raw pointer
+    explicit unique_ptr(T* other)
+        : m_ptr{other} {}
+
+    explicit unique_ptr()
+        : m_ptr{nullptr} {}
+
+    // Return managed pointer
+    T* get() const { return m_ptr; }
+
+    // Release ownership of pointer (no longer automatically managed)
     T* release() {
         auto old_p = m_ptr;
         m_ptr = nullptr;
@@ -45,35 +41,29 @@ public:
     }
 
     // Free managed object
-    ~unique_ptr() {    
-        delete m_ptr;    
-    }   
+    ~unique_ptr() { delete m_ptr; }
 
     // Point to another object, freeing the old one
-    // If no pointer is provided 
+    // If no pointer is provided
     void reset(T* new_ptr = nullptr) {
-        if (m_ptr) 
-            delete m_ptr;  
+        if (m_ptr)
+            delete m_ptr;
         m_ptr = new_ptr;
     }
 
-    T& operator*() const {
-        return *m_ptr;
-    }
+    T& operator*() const { return *m_ptr; }
 
-    T* operator->() const {
-        return m_ptr;
-    }
+    T* operator->() const { return m_ptr; }
 
     // Move assignment
     unique_ptr& operator=(unique_ptr<T>&& other) {
         // Prevent self-move
         if (this != &other) {
-          this->reset(other.get());
-          other.m_ptr = nullptr;
+            this->reset(other.get());
+            other.m_ptr = nullptr;
         }
         return *this;
-    }    
+    }
 
     std::nullptr_t operator=(std::nullptr_t) {
         delete m_ptr;
@@ -82,37 +72,23 @@ public:
     }
 
     // Comparison operators overload
-    bool operator==(const unique_ptr<T>& p) const {
-        return this->get() == p.get();
-    }
+    bool operator==(const unique_ptr<T>& p) const { return this->get() == p.get(); }
 
-    bool operator!=(const unique_ptr<T>& p) const {
-        return this->get() != p.get();
-    }
+    bool operator!=(const unique_ptr<T>& p) const { return this->get() != p.get(); }
 
-    bool operator>=(const unique_ptr<T>& p) const {
-        return this->get() >= p.get();
-    }
+    bool operator>=(const unique_ptr<T>& p) const { return this->get() >= p.get(); }
 
-    bool operator<=(const unique_ptr<T>& p) const {
-        return this->get() <= p.get();
-    }
+    bool operator<=(const unique_ptr<T>& p) const { return this->get() <= p.get(); }
 
-    bool operator>(const unique_ptr<T>& p) const {
-        return this->get() > p.get();
-    }
+    bool operator>(const unique_ptr<T>& p) const { return this->get() > p.get(); }
 
-    bool operator<(const unique_ptr<T>& p) const {
-        return this->get() < p.get();
-    }
-
-
+    bool operator<(const unique_ptr<T>& p) const { return this->get() < p.get(); }
 };
-    template<typename T, typename ... Args>
-    [[nodiscard]] unique_ptr<T> 
-    make_unique(Args&& ... args) {
-        auto ptr = new T(std::forward<Args>(args)...); 
-        return unique_ptr<T>(ptr);
-    }
 
-} // end namespace
+template <typename T, typename... Args>
+[[nodiscard]] unique_ptr<T> make_unique(Args&&... args) {
+    auto ptr = new T(std::forward<Args>(args)...);
+    return unique_ptr<T>(ptr);
+}
+
+} // namespace lib
