0365c3030cec9d81f5a559cc4005a62ca03273ca
fyz.m <237607300+fyz-m@users.noreply.github.com>
2026-06-23 11:07:04 +0400

    Added tests

---
+++ shared_ptr/shared_ptr.hpp
@@ -0,0 +1,166 @@
+#include <cstddef>
+#include <print>
+#include <string>
+#include <utility>
+
+namespace lib {
+
+template<typename T>
+class shared_ptr {
+
+    private:
+        T* m_Ptr{};
+        size_t* m_Ptr_count{};
+
+    public:
+
+        shared_ptr() : m_Ptr{nullptr}, m_Ptr_count{new size_t(1)} {}
+
+        explicit shared_ptr(T* other) 
+                  : m_Ptr{other}, m_Ptr_count(new size_t(1)) 
+                    {}
+
+        // Copy constructor
+        shared_ptr(const shared_ptr& other) 
+                  : m_Ptr{other.get()}, m_Ptr_count(other.m_Ptr_count) 
+                  {
+                    incrementCount();
+                  }
+        
+        // Move constructor
+        shared_ptr(shared_ptr&& other)
+                  : m_Ptr(other.get()), m_Ptr_count(other.m_Ptr_count) 
+                  {
+                    // Set to nullptr so other's destructor doesn't decrement count
+                    other.m_Ptr_count = nullptr;
+                    other.m_Ptr = nullptr;
+                  }
+
+        
+        ~shared_ptr() {
+            if (m_Ptr_count && --(*m_Ptr_count) == 0) {
+                delete m_Ptr;
+                delete m_Ptr_count;
+            }
+        }
+
+        // Copy assignment
+        shared_ptr& operator=(const shared_ptr& other) {
+            if (this != &other) {
+                reset();            // Release ownership of current ptr
+                copyFrom(other);    // Copy members of other
+                incrementCount();   
+            }
+            return *this;           
+        }
+
+        // Move assignment
+        shared_ptr& operator=(shared_ptr&& other) {
+            if (this != &other) {
+            reset();
+            copyFrom(other);
+            other.m_Ptr = nullptr;
+            other.m_Ptr_count = nullptr;
+            }
+            return *this;
+        }
+
+        T* get() const {
+            return m_Ptr;
+        }
+
+        // Release ownership and set members to null
+        void reset() {
+
+            // If already nullptr
+            if (!m_Ptr_count)
+                return; 
+
+            // If *this is the only ptr to obj
+            else if (isUnique()) {
+                delete m_Ptr;
+                delete m_Ptr_count;
+            }
+            else 
+                --*m_Ptr_count;
+
+            // Set to nullptr so destructor is avoided
+            m_Ptr_count = nullptr;
+            m_Ptr = nullptr;
+        }
+
+        // Release and own another ptr
+        void reset(T* other) {
+
+            if (m_Ptr_count) {                 
+                if (isUnique()) {
+                    delete m_Ptr;
+                    delete m_Ptr_count;
+                }
+                else 
+                    --*m_Ptr_count;
+            }
+            m_Ptr = other;
+            m_Ptr_count = new size_t{1};
+        }
+
+        // Swap resources wih another shared_ptr
+        void swap(shared_ptr& other) {
+            auto this_ptr = m_Ptr;
+            auto this_Ptr_count = m_Ptr_count;
+            copyFrom(other);
+
+            other.m_Ptr = this_ptr;
+            other.m_Ptr_count = this_Ptr_count;
+        }
+
+        size_t useCount() const {
+            if (m_Ptr_count)
+                return *m_Ptr_count;
+            return 0;
+        }
+
+        bool isUnique() const {
+            return useCount() == 1;
+        }
+
+        T& operator*() const {
+            return *m_Ptr;
+        }
+
+        T* operator->() const {
+            return m_Ptr;
+        }
+
+        explicit operator bool() const {
+            return m_Ptr;
+        }
+
+private:
+        // Copy members of other shared_ptr
+        void copyFrom(const shared_ptr& other) {
+            this->m_Ptr = other.m_Ptr;
+            this->m_Ptr_count = other.m_Ptr_count;
+        }
+
+
+        void incrementCount() {
+            if (m_Ptr_count) {
+                ++*m_Ptr_count;
+            }   
+        }
+
+        void decrementCount() {
+            if (m_Ptr_count)
+                --*m_Ptr_count;
+            }
+
+        
+ };
+
+template<typename T, typename ...Args>
+[[nodiscard]] shared_ptr<T> make_shared(Args&& ...args) {
+    return shared_ptr(new T(std::forward<Args>(args)...));
+ }
+
+} // end namespace
---
+++ shared_ptr/test_shared.cpp
@@ -0,0 +1,177 @@
+#include "shared_ptr.hpp"
+#include <cassert>
+
+void test();
+
+void test_copy_constructor();
+
+void test_move_constructor();
+
+void test_copy_assignment();
+
+void test_move_assignment();
+
+
+struct TestDummy {
+    bool moved {false};
+    bool copied {false};
+    bool& destroyed;
+
+    TestDummy(bool& destroyed) : copied(false), moved(false), destroyed(destroyed) {}
+
+    TestDummy(const TestDummy&, bool& destroyed) : copied{true}, destroyed{destroyed} {}
+    TestDummy(const TestDummy&&, bool& destroyed) : moved{true}, destroyed{destroyed} {}
+
+    TestDummy& operator=(const TestDummy& t)  { 
+        copied = true; 
+        destroyed = t.destroyed;
+        return *this;
+    }; 
+    TestDummy& operator=( TestDummy&& t) {
+        moved = true; 
+        destroyed = t.destroyed;
+        return *this;
+     };
+    
+    ~TestDummy() {
+        destroyed = true;
+    }
+};
+
+int main() {
+    test();   
+}
+
+void test() {
+    test_copy_constructor();
+    test_move_constructor();
+    test_copy_assignment();
+    test_move_assignment();
+}
+
+void test_copy_constructor() {
+    bool destroyed {false};
+    {
+        auto test_dummy1 = lib::make_shared<TestDummy>(destroyed);
+        {
+            auto test_dummy2 {test_dummy1};
+            assert(test_dummy1.useCount() == test_dummy2.useCount() && test_dummy1.useCount() == 2);
+        }
+        assert(destroyed == false);
+        assert(test_dummy1.isUnique());
+    } assert(destroyed);
+}
+
+void test_move_constructor() {
+    {
+        bool destroyed {false};
+        {
+            auto test_dummy1 = lib::make_shared<TestDummy>(destroyed);
+            auto test_dummy2 {std::move(test_dummy1)};
+            assert(test_dummy2.useCount() == 1);
+        } assert(destroyed);
+    }
+
+    {
+        bool destroyed {false};
+        {
+            auto test_dummy1 = lib::make_shared<TestDummy>(destroyed);
+            auto test_dummy2 {std::move(test_dummy1)};
+            auto test_dummy3 {std::move(test_dummy2)};
+            assert(test_dummy3.useCount() == 1);
+        } assert(destroyed);
+    }
+}
+
+void test_copy_assignment() {
+    {
+        bool destroyed {false};
+        {
+            auto test_dummy1 = lib::make_shared<TestDummy>(destroyed);
+            {
+                auto test_dummy2 = test_dummy1;
+                assert(test_dummy2.useCount() == 2);
+                assert(test_dummy2.get() == test_dummy1.get());
+            }
+            assert(test_dummy1.isUnique());
+        } assert(destroyed);
+    }
+
+    // Test chained copy assignments
+    {
+        bool destroyed {false};
+        {
+            auto test_dummy1 = lib::make_shared<TestDummy>(destroyed);
+            {
+                lib::shared_ptr<TestDummy> test_dummy2;
+                auto test_dummy3 = test_dummy2 = test_dummy1;
+                assert(test_dummy2.useCount() == 3);
+                assert(test_dummy3.get() == test_dummy2.get() && test_dummy2.get() == test_dummy1.get());
+            }
+            assert(test_dummy1.isUnique());
+        } assert(destroyed);
+    }
+
+    // Test copying into existed shared releases its resources
+    {
+        bool d1_destroyed {false};
+        bool d2_destroyed {false};
+        {
+            auto test_dummy1 = lib::make_shared<TestDummy>(d1_destroyed);
+            {
+                auto test_dummy2 = lib::make_shared<TestDummy>(d2_destroyed);
+                test_dummy1 = test_dummy2;
+                assert(d1_destroyed); 
+                assert(test_dummy2.useCount() == 2);
+            } 
+        } assert(d2_destroyed);
+    }
+
+}
+
+void test_move_assignment() {
+    {
+        bool destroyed {false};
+        {
+            auto test_dummy1 = lib::make_shared<TestDummy>(destroyed);
+            auto test_dummy1_ptr = test_dummy1.get();
+            {
+                auto test_dummy2 = std::move(test_dummy1);
+                assert(test_dummy2.useCount() == 1);
+                assert(test_dummy2.get() == test_dummy1_ptr);
+            }
+            assert(destroyed);
+        } 
+    }
+
+    // Test chained move assignments
+    {
+        bool destroyed {false};
+        {
+            auto test_dummy1 = lib::make_shared<TestDummy>(destroyed);
+            {
+                lib::shared_ptr<TestDummy> test_dummy2;
+                lib::shared_ptr<TestDummy> test_dummy3;
+                test_dummy3 = std::move(test_dummy2 = std::move(test_dummy1));
+                assert(test_dummy3.useCount() == 1);
+            }
+            assert(destroyed);
+        } 
+    }
+
+    // Test moving into existed shared_ptr releases its resources
+    {
+        bool d1_destroyed {false};
+        bool d2_destroyed {false};
+        {
+            auto test_dummy1 = lib::make_shared<TestDummy>(d1_destroyed);
+            {
+                auto test_dummy2 = lib::make_shared<TestDummy>(d2_destroyed);
+                test_dummy1 = std::move(test_dummy2);
+                assert(d1_destroyed); 
+                assert(test_dummy1.useCount() == 1);
+            }
+            assert(d2_destroyed == false);
+        }  assert(d2_destroyed); 
+    }
+}
