b5537b5c9f34f50d5acb11976e8ce19cac92f007
fyz.m <237607300+fyz-m@users.noreply.github.com>
2026-06-24 19:20:16 +0400

    Change member to be stack allocated

--- array/array.hpp
+++ array/array.hpp
@@ -1,76 +1,53 @@
-
 #include <cstddef>
 #include <stdexcept>
-namespace lib {
 
+namespace lib {
 template <typename T, size_t N>
 class Array {
 
   private:
-    T* m_Buffer{new T[N]};
+    T m_Buffer[N];
 
   public:
-    Array() = default;
-
-    // Copy constructor
-    Array(const Array<T, N>& other)
-        : m_Buffer(new T[N]) {
-        copyElementsFrom(other);
-    }
-
-    // Copy assignment
-    Array& operator=(const Array<T, N>& other) {
-        if (this == &other) {
-            return *this;
-        }
-
-        // delete old array
-        delete[] m_Buffer;
-        m_Buffer = new T[N];
-
-        copyElementsFrom(other);
-        return *this;
-    }
-
-    // Move constructor
-    Array(Array&& other) noexcept
-        : m_Buffer{other.m_Buffer} {
-        other.m_Buffer = nullptr;
-    }
-
-    // Move assignment
-    Array& operator=(Array&& other) noexcept {
-        if (this == &other) {
-            return *this;
-        }
-
-        delete[] m_Buffer;
-        m_Buffer = other.m_Buffer;
-        other.m_Buffer = nullptr;
-        return *this;
-    }
-
-    T& operator[](size_t index) const {
+    T& operator[](const size_t index) {
         return m_Buffer[index];
     }
 
-    ~Array() {
-        delete[] m_Buffer;
+    const T& operator[](const size_t index) const {
+        return m_Buffer[index];
     }
 
-    T* data() const {
+    T* data() {
         return m_Buffer;
     }
 
-    T& front() const {
+    const T* data() const {
+        return m_Buffer;
+    }
+
+    T& front() {
         return m_Buffer[0];
     }
 
-    T& back() const {
+    const T& front() const {
+        return m_Buffer[0];
+    }
+
+    T& back() {
         return m_Buffer[N - 1];
     }
 
-    T& at(int index) const {
+    const T& back() const {
+        return m_Buffer[N - 1];
+    }
+
+    T& at(const int index) {
+        if (index < 0 || index >= N)
+            throw std::out_of_range("Out of bounds array access.");
+        return m_Buffer[index];
+    }
+
+    const T& at(const int index) const {
         if (index < 0 || index >= N)
             throw std::out_of_range("Out of bounds array access.");
         return m_Buffer[index];
@@ -84,29 +61,22 @@ class Array {
 
     void swap(Array<T, N>& other) noexcept {
         for (int i = 0; i < N; ++i) {
-            auto temp = m_Buffer[i];
-            m_Buffer[i] = other[i];
-            other[i] = temp;
+            auto temp = std::move(m_Buffer[i]);
+            m_Buffer[i] = std::move(other[i]);
+            other[i] = std::move(temp);
         }
     }
 
-    [[nodiscard]] size_t size() const {
+    [[nodiscard]] constexpr size_t size() const {
         return N;
     }
 
-    [[nodiscard]] size_t max_size() const {
+    [[nodiscard]] constexpr size_t max_size() const {
         return N;
     }
 
-    [[nodiscard]] bool empty() const {
+    [[nodiscard]] constexpr bool empty() const {
         return N == 0;
-    }
-
-  private:
-    void copyElementsFrom(const Array<T, N>& other) const {
-        for (int i = 0; i < N; i++) {
-            m_Buffer[i] = other.m_Buffer[i];
-        }
     }
 };
 } // namespace lib
